a3187bc6df15a23b2ce011ade460a410832e9032
donnadonna123 <bay12@yahoo.com>
2025-10-27 17:10:19 -0400

    mona

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,7 +4,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'videoStorage',
   access: (allow) => ({
-    'videos/public/': [allow.guest.to(['read']), allow.authenticated.to(['read', 'write'])],
+    'videos/public/*': [allow.guest.to(['read']), allow.authenticated.to(['read', 'write'])],
     'videos/private/{identityId}/': [allow.entity('identity').to(['read', 'write'])],
   }),
 });
