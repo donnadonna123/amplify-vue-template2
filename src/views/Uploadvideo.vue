4763bcb80cc621cd54ebd4cf8900a46875630699
donnadonna123 <bay12@yahoo.com>
2025-10-28 14:01:53 -0400

    tueD

--- src/views/Uploadvideo.vue
+++ src/views/Uploadvideo.vue
@@ -1,13 +1,40 @@
-<script setup lang="ts">  
-const props = defineProps<{
-    multiple?: boolean;  
-}>();
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { uploadData } from 'aws-amplify/storage'
+
+const file = ref<File | null>(null)
+const router = useRouter()
+
+const handleFileChange = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  if (target.files && target.files.length > 0) {
+    file.value = target.files[0]
+  }
+}
+
+const handleUpload = async () => {
+  if (!file.value) return
+
+  try {
+    await uploadData({
+      key: `uploads/${file.value.name}`,
+      data: file.value,
+      options: { accessLevel: 'public' },
+    }).result
+
+    console.log('Upload successful')
+ 
+  } catch (error) {
+    console.error('Upload failed:', error)
+  }
+}
 </script>
-<template> 
-<br><br>
-    <input type="file" :multiple="multiple" />
-    <label for="file-input" class="btn">
-     Upload File
-</label>
-<input id="file-input" type="file" hidden />
+
+<template>
+  <div>
+    <input type="file" @change="handleFileChange" />
+    <button @click="handleUpload">Upload to S3</button>
+  </div>
 </template>
