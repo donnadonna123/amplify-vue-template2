190558467cd3550767a28586db8f84974896e701
donnadonna123 <bay12@yahoo.com>
2025-10-28 20:41:25 -0400

    tueH

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,8 +14,7 @@ import Whowearestory from "@/views/Whowearestory.vue";
 import Whoweareteam from "@/views/Whoweareteam.vue";
 import Todos from "@/views/Todos.vue";
 import ResetPassword from "@/views/ResetPassword.vue";
-import Uploadvideo from "@/views/Uploadvideo.vue";
-import Videolist from "@/views/Videolist.vue";
+ 
 
 import { getCurrentUser } from 'aws-amplify/auth';
 
@@ -60,11 +59,6 @@ const routes = [
           path: '/Uploadvideo',
           name: 'Uploadvideo',
           component: Uploadvideo
-      },
-      {
-          path: '/Videolist',
-          name: 'Videolist',
-          component: Videolist
       },
       {
         path: '/login',
