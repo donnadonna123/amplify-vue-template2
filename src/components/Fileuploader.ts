30f3ef843b15337adc1fe8339f37926e7ff330dc
donnadonna123 <bay12@yahoo.com>
2025-10-30 18:43:18 -0400

    thurE

--- src/components/Fileuploader.ts
+++ src/components/Fileuploader.ts
@@ -1,13 +1,1 @@
-import { FileUploader } from '@aws-amplify/ui-react-storage';
-import '@aws-amplify/ui-react/styles.css';
-
-export const DefaultFileUploaderExample = () => {
-  return (
-    <FileUploader
-      acceptedFileTypes={['video/*']}
-      path="public/"
-      maxFileCount={1}
-      isResumable
-    />
-  );
-};
+ 
