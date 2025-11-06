ec0e502a04dd96fa899c8b3960b498c1ab938961
donnadonna123 <bay12@yahoo.com>
2025-11-05 19:04:49 -0500

    wedg

--- src/views/Huangstory.vue
+++ src/views/Huangstory.vue
@@ -1,5 +1,5 @@
 <template>
   <div style="margin: auto; width: 80%;">
-  <img style="margin: auto; width: 100%;" src="/images/Huangstory.jpg" alt="Whoweare Image"  >
+  <img style="margin: auto; width: 100%;" src="/images/huangstory.jpg" alt="Whoweare Image"  >
     </div>
 </template>
